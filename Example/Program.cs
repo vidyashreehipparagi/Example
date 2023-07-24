39284b40e0ea75a65e39cf51e68c36af2cb4e2b4
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-24 13:45:01 +0530

    Add project files.

---
+++ Example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(10,"India");
+            hashtable.Add(91, "IND");
+            hashtable.Add(63, "AUS");
+            hashtable.Add(1, "USA");
+            hashtable.Remove(63);
+
+            foreach(DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine(entry.Key+" "+entry.Value);
+            }
+
+
+
+            Quee1 queue = new Quee1();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            queue.Dequeue();//remove first element
+            Console.WriteLine($"first element in the list{queue.Peek()}");
+            Console.WriteLine($"total elements in the queue{queue.Count()}");
+           
+
+        }
+
+        }
+}
---
+++ Example/Quee1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Example
+{
+    internal class Quee1
+    {
+        public Quee1()
+        {
+        }
+
+        internal object Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Enqueue(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Peek()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
